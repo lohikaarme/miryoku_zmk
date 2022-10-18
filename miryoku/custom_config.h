d2db85f8fb8b975344a012c51bc12f7ce73699a6
lox <96849372+lohikaarme@users.noreply.github.com>
2022-10-17 21:48:59 -0700

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_CLIPBOARD_WIN
+#define MIRYOKU_MAPPING_THUMB_COMBOS
+#define MIRYOKU_KLUDGE_MOUSEKEYSPR
